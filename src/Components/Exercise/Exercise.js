f44c9e28455cb3f050631313801cfe6e01cb275e
rakibul58 <rhrahi14@gmail.com>
2022-09-29 00:58:49 +0600

    card style added

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -7,15 +7,15 @@ const Exercise = (props) => {
     <div className="exercise col">
       <div className="border pb-3 rounded-3">
         <div className="p-2">
-          <img src={img} className="img-thumbnail" alt="..." />
+          <img src={img} className="img-thumbnail rounded-4" alt="..." />
         </div>
         <div className="px-3 excercise-details">
-          <h5>{name}</h5>
-          <p>
+          <h5 className="text-success fw-bold">{name}</h5>
+          <p className="fw-semibold text-secondary">
             <small>{details.slice(0, 90)}...</small>
           </p>
-          <p>For Age : {age}</p>
-          <p>Time required : {time}s</p>
+          <p className="m-0">For Age : <span className="fw-semibold">{age}</span></p>
+          <p>Time required : <span className="fw-semibold text-success">{time}s</span></p>
           <button className="btn btn-success w-100">Add to list</button>
         </div>
       </div>
