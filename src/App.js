c26afe9457baa05ad05e3f4b4dbdac9bfecddc1f
rakibul58 <rhrahi14@gmail.com>
2022-09-28 21:26:51 +0600

    fake data added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { useEffect} from 'react';
 
 function App() {
+ 
+  useEffect(()=>{
+    fetch('exercises.json')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  },[])
   return (
     <div className="App">
-      
+      <h1>Heloo</h1>
     </div>
   );
 }
