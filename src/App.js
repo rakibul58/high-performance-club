4f06da14dff8a254d0759f9f90441507e591f457
rakibul58 <rhrahi14@gmail.com>
2022-09-29 12:58:45 +0600

    questions added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Exercises from "./Components/Exercises/Exercises";
+import Questions from "./Components/Questions/Questions";
 
 
 function App() {
   return (
     <div className="App">
       <Exercises></Exercises>
+      <Questions></Questions>
     </div>
   );
 }
---
+++ src/Components/Questions/Questions.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+const Questions = () => {
+  return (
+    <div className="mt-5 container">
+      <h2 className="text-success fw-semibold">Frequently Asked Questions</h2>
+      <div className="my-5">
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseOne"
+                aria-expanded="false"
+                aria-controls="flush-collapseOne"
+              >
+                How Does React Work?
+              </button>
+            </h2>
+            <div
+              id="flush-collapseOne"
+              class="accordion-collapse collapse"
+              aria-labelledby="flush-headingOne"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+              React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseTwo"
+                aria-expanded="false"
+                aria-controls="flush-collapseTwo"
+              >
+                What is the Difference Between Props and State?
+              </button>
+            </h2>
+            <div
+              id="flush-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="flush-headingTwo"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+              Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseThree"
+                aria-expanded="false"
+                aria-controls="flush-collapseThree"
+              >
+                What are the uses of useEffect other than load API data?
+              </button>
+            </h2>
+            <div
+              id="flush-collapseThree"
+              class="accordion-collapse collapse"
+              aria-labelledby="flush-headingThree"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+              useEffect uses are :-
+                    <br />1.Running on state change: validating input field
+                    <br />2.Running on state change: live filtering
+                    <br />3.Running on state change: trigger animation on new array value
+                    <br />4.Running on props change: update paragraph list on fetched API data update
+                    <br />5.Running on props change: updating fetched API data to get BTC updated price
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
